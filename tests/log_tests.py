b345f3667baaa6fa80bfa8eac6c498af2c3037be
William Glass <william.glass@gmail.com>
2015-08-30 18:42:40 -0700

    Fix test broken in previous patch.

--- tests/log_tests.py
+++ tests/log_tests.py
@@ -13,7 +13,7 @@ class LogTests(unittest.TestCase):
     @patch("lighthouse.log.CLIHandler")
     @patch("lighthouse.log.logging")
     def test_setup_adds_handler_to_root_logger(self, mock_logging, CLIHandler):
-        log.setup()
+        log.setup("foobar")
 
         mock_logging.getLogger.assert_called_once_with()
         mock_logging.getLogger.return_value.addHandler.assert_called_once_with(
